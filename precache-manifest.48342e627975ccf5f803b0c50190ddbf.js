dbd87550a54ba66de8326606cf3b4ee301c318af
Orion-P <orionpoptean@gmail.com>
2020-08-17 16:34:04 -0700

    Updates

---
+++ precache-manifest.48342e627975ccf5f803b0c50190ddbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54bebb3621ace86cedc814d70906bae0",
+    "url": "/video-browser/index.html"
+  },
+  {
+    "revision": "ee32281f6ce7b03281d2",
+    "url": "/video-browser/static/css/main.96c11df6.chunk.css"
+  },
+  {
+    "revision": "9de3ff0d6ded0777abac",
+    "url": "/video-browser/static/js/2.48f3b681.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/video-browser/static/js/2.48f3b681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee32281f6ce7b03281d2",
+    "url": "/video-browser/static/js/main.2c8cc75f.chunk.js"
+  },
+  {
+    "revision": "0586d4bf0bb746434afb",
+    "url": "/video-browser/static/js/runtime-main.56fb1b04.js"
+  }
+]);
